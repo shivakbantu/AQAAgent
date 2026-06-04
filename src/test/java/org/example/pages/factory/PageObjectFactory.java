3f1ee9c7f69c31392792142540204e9cd4d4aaf6
shivakbantu <shivabantuk@gmail.com>
2026-06-04 09:51:40 +0530

    Create src/test/java/org/example/pages/factory/PageObjectFactory.java

---
+++ src/test/java/org/example/pages/factory/PageObjectFactory.java
@@ -0,0 +1,28 @@
+package org.example.pages.factory;
+
+import org.example.pages.DashboardPage;
+import org.example.pages.OrangeHrmLoginPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * Factory pattern for page object creation.
+ */
+public final class PageObjectFactory {
+
+    private final WebDriver driver;
+    private final WebDriverWait wait;
+
+    public PageObjectFactory(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+    }
+
+    public OrangeHrmLoginPage loginPage() {
+        return new OrangeHrmLoginPage(driver, wait);
+    }
+
+    public DashboardPage dashboardPage() {
+        return new DashboardPage(driver, wait);
+    }
+}
